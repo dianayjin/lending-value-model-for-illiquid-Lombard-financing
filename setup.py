a936eda92a762051729a5515f94c2ce3419551db
dianayjin <51413640+dianayjin@users.noreply.github.com>
2024-02-22 17:33:01 +0100

    Initial directory loading.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='The goal of this model is to understand the credit risk inherent in loans collateralized by liquid assets (e.g. stocks, bonds, funds) and to investigate the liquidity (size) effects on the riskiness of the transaction.',
+    author='Diana Jin',
+    license='MIT',
+)
